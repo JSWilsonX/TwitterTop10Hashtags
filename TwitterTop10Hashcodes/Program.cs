53c2101c01141d87f3607c4a3ba3f3228f470f38
James Wilson <jswilson@alum.mit.edu>
2023-02-10 10:09:13 -0500

    Stream and Process in Parallel

--- TwitterTop10Hashcodes/Program.cs
+++ TwitterTop10Hashcodes/Program.cs
@@ -3,12 +3,15 @@
 //-----------------------------------------------------------------------
 
 using TwitterTop10Hashtags;
+using System.Collections.Concurrent;
 
 var twitterStreamProcessor = new ProcessTwitterStream();
 var hashtags = new Hashtags();
 var logFrequency = 1;  // In minutes
 var timeToLog = DateTime.Now.AddMinutes(logFrequency);
 var lastTweetCount = 0;
+var tweetMessageQueue = new ConcurrentQueue<string>();
+var idleTime = 0;
 
 Console.WriteLine($"Time: {DateTime.Now:g}");
 Console.WriteLine($"Starting to log every {logFrequency} minute(s)...");
@@ -16,15 +19,31 @@ Console.WriteLine();
 
 try
 {
+    var processingTask = Task.Run(async () => { 
+        while (true)
+        {
+            while (!tweetMessageQueue.IsEmpty)
+            {
+                if (tweetMessageQueue.TryDequeue(out string? tweetMessage))
+                {
+                    hashtags.UpdateHashtagCounts(tweetMessage);
+                    LogStatistics();
+                }
+            }
+            idleTime++;
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    });
+
     await twitterStreamProcessor.GetTweets((Action<TweetObject>)((tweet) =>
     {
         var tweetMessage = tweet?.Data?.Text ?? "";
+        tweetMessageQueue.Enqueue(tweetMessage);
         //Console.WriteLine($"ID: {tweet?.Data?.Id}");
         //Console.WriteLine(tweetMessage);
 
-        hashtags.UpdateHashtagCounts(tweetMessage);
-        LogStatistics();
     }));
+    await processingTask;
 }
 catch (Exception requestException)
 {
@@ -37,8 +56,9 @@ void LogStatistics()
     if (timeToLog < DateTime.Now)
     {
         var newTweetCount = hashtags.GetNumberOfTweets();
+        var queueLength = tweetMessageQueue.Count;
         timeToLog = DateTime.Now.AddMinutes(logFrequency);
-        Console.WriteLine($"Time: {DateTime.Now:g}");
+        Console.WriteLine($"Time: {DateTime.Now:g}  Queue Length: {queueLength}  Idle Time: {idleTime}");
         Console.WriteLine($"Total number of tweets received: {newTweetCount:n0} New: {newTweetCount - lastTweetCount:n0}");
         Console.WriteLine($"Total number of hashtags received: {hashtags.GetNumberOfHashtags():n0}");
         foreach (var hashtag in hashtags.GetTopHashtags().ToList())
@@ -48,5 +68,6 @@ void LogStatistics()
         }
         Console.WriteLine();
         lastTweetCount = newTweetCount;
+        idleTime = 0;
     }
 }
